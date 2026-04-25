fd708636f2f12b1db977fe56791aec9e46a61183
Abderrahman Zerouali <lopikola11@gmail.com>
2026-04-25 02:19:08 +0200

    final clean jsx version ready for deploy

---
+++ ecommerce-project-ts/components/utils/explain.jsx
@@ -0,0 +1,12 @@
+first thing i want to tell you that to use redux toolkit its mendatory to understand redux library first 
+because there are many things runs behind the seans like immer library which 
+allow you to change the state without mutation even it looks like ur mutating the state
+
+rdux toolkit is a life savior and much secure even i prfere using zustand which 
+is much smaller, redux toolkit creat the action types for  you 
+in zustand we control everything but for redux we dont directly update the state
+but redux do that for us when it recevies dispach method that adresss the reducer 
+to handle the state based on the action type 
+redux ensures predictiblity :) 
+
+lets start making the store , i already did that i ll show u how  
---
+++ ecommerce-project-ts/components/utils/header.jsx
@@ -0,0 +1,75 @@
+import { useEffect } from "react";
+import { Link } from "react-router";
+import { useSelector,useDispatch } from "react-redux";
+import { setQuantity } from "../../store/slice.jsx";
+export function Header() {
+  const checkouts = useSelector((state) => state.cart.checkouts);
+  const quantity = useSelector((state) => state.cart.quantity);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (!checkouts || checkouts.length === 0) {
+      dispatch(setQuantity(0));
+      return;
+    }
+
+    let total = 0;
+    checkouts.forEach((item) => {
+      total += item.quantity;
+    });
+
+    dispatch(setQuantity(total));
+  }, [checkouts, setQuantity, dispatch]);
+
+  return (
+    <div className="header">
+      <div className="left-section">
+        <Link to="/" className="header-link">
+          <div
+            style={{
+              fontSize: "28px",
+              fontWeight: "bold",
+              color: "white",
+              letterSpacing: "1px",
+            }}
+          >
+            Abdou Artist
+          </div>
+        </Link>
+      </div>
+
+      <div className="middle-section">
+        <input className="search-bar" type="text" placeholder="Search" />
+        <button className="search-button">
+          <img
+            className="search-icon"
+            src="/images/icons/search-icon.png"
+            alt="search"
+          />
+        </button>
+      </div>
+
+      <div className="right-section">
+        <Link className="orders-link header-link" to="/order">
+          <span className="orders-text">Orders</span>
+        </Link>
+
+        {/* CART */}
+        <Link className="cart-link header-link" to="/cheackout">
+          <img
+            className="cart-icon"
+            src="/images/icons/cart-icon.png"
+            alt="cart"
+          />
+
+          {/* FIXED: always show 0 instead of empty */}
+          <div className="cart-quantity">
+            {quantity ?? 0}
+          </div>
+
+          <div className="cart-text">Cart</div>
+        </Link>
+      </div>
+    </div>
+  );
+}
