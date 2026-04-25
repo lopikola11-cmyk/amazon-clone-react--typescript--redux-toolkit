1721f92ed523cad6fb61205765671c07ed11df44
Abderrahman Zerouali <lopikola11@gmail.com>
2026-04-25 17:54:34 +0200

    fix router imports

--- ecommerce-project-ts/src/main.jsx
+++ ecommerce-project-ts/src/main.jsx
@@ -1,7 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'import { Provider } from 'react-redux'; 
+import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'; 
 import './index.css'
 import { store } from '../store/store.jsx';
 
