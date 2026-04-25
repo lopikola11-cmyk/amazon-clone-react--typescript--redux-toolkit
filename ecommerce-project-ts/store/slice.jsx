6f5d5167682b72af6fbedd6293a0a57b749f3544
abderrahman zerouali <lopikola11@gmail.com>
2026-04-25 16:24:33 +0200

    Refactor cart slice by removing unused types and comments

--- ecommerce-project-ts/store/slice.jsx
+++ ecommerce-project-ts/store/slice.jsx
@@ -1,71 +1,38 @@
-
-
-import {createSlice, createAsyncThunk} from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
-/*
-type Rating = {
-  stars: number;
-  count: number;
+
+const API = "https://amazon-clone-react-typescript-redux-3lcl.onrender.com";
+
+const initialState = {
+  checkouts: [],
+  quantity: null,
 };
 
-type Product = {
-  id: string;
-  image: string;
-  name: string;
-  keywords: string[];
-  rating: Rating;
-  priceCents: number;
-  createdAt: string;
-  updatedAt: string;
-};
+export const fetchCheckouts = createAsyncThunk(
+  'cart/fetchCheckouts',
+  async () => {
+    const response = await axios.get(`${API}/api/cart-items?expand=product`);
+    return response.data;
+  }
+);
 
-export type Checkout = {
-  id: number;
-  productId: string;
-  quantity: number;
-  deliveryOptionId: string;
-  createdAt: string;
-  updatedAt: string;
-  product: Product;
-};
-
-type CartState = {
-    checkouts: Checkout[];
-    quantity: number | null;
-};*/
-const initialState/*: CartState*/ = {
-    checkouts: [],
-    quantity: null,
-};
-
-export const fetchCheckouts = createAsyncThunk /*<Checkout[]*/>('cart/fetchCheckouts', async () => {
-    const response = await axios.get('/api/cart-items?expand=product');
-    return response.data /*as Checkout[]*/;
-});
-
-
-
-
- const cartSlice = createSlice({
-    name: 'cart',
-    initialState,
-    reducers: {
-
-        setCheckouts: (state, action) => {
-            state.checkouts = action.payload;
-        },
-        setQuantity: (state, action) => {
-            state.quantity = action.payload;
-        },
+const cartSlice = createSlice({
+  name: 'cart',
+  initialState,
+  reducers: {
+    setCheckouts: (state, action) => {
+      state.checkouts = action.payload;
     },
-    extraReducers: (builder) => {
-        builder.addCase(fetchCheckouts.fulfilled, (state, action) => {
-            state.checkouts = action.payload;
-        });
-        },
-
-
- 
+    setQuantity: (state, action) => {
+      state.quantity = action.payload;
+    },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(fetchCheckouts.fulfilled, (state, action) => {
+      state.checkouts = action.payload;
+    });
+  },
 });
+
 export const { setCheckouts, setQuantity } = cartSlice.actions;
 export const cartReducer = cartSlice.reducer;
