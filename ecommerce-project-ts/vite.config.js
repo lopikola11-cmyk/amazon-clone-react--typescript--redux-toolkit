6ba0e239cdc616e9931643a744ef25014fc09425
abderrahman zerouali <lopikola11@gmail.com>
2026-04-25 03:19:24 +0200

    Simplify Vite configuration by removing Babel plugin

--- ecommerce-project-ts/vite.config.js
+++ ecommerce-project-ts/vite.config.js
@@ -1,18 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react({
-  babel: {
-    plugins: [['babel-plugin-react-compiler', { target: '19' }]],
-  },
-})],
- server:{ 
-  proxy:{ 
-    '/api':'http://localhost:3000',
-    '/images':'http://localhost:3000'
-   
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/api': 'http://localhost:3000',
+      '/images': 'http://localhost:3000'
+    }
   }
- }
 })
